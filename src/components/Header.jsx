c993df538d21f31b31a97d1174264927b3f422ef
Santhos <santhoshsunthar@gmail.com>
2023-01-13 17:33:04 +0530

    Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+    )
+}
+
+export default Header
