3e386664d9a04c9b4df1d6281e7b44a5048ef293
Santhos <santhoshsunthar@gmail.com>
2023-02-01 18:35:18 +0530

    Added react icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
-import CreatePost from './CreatePost'
+import React, { useState } from 'react';
+import CreatePost from './CreatePost';
+import { HiMenu } from "react-icons/hi";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 const NavBar = () => {
-    const [view, setView] = useState(false);    
-    const [btnText, setBtnText] = useState('+');
+    const [view, setView] = useState(false);
+    const [btnIconText, setBtnIconText] = useState('IoIosAddCircleOutline');
 
     const showCreatePost = () => {
         setView(!view);
 
         if (view !== true) {
-            setBtnText('x');
+            setBtnIconText('AiOutlineCloseCircle');
         } else {
-            setBtnText('+');
+            setBtnIconText('IoIosAddCircleOutline');
         }
     }
 
     return (
         <>
-        <nav className="navbar container-fluid">
+            <nav className="navbar container-fluid">
+                <HiMenu size={30} />
                 <span className="navbar-brand mb-0 pl-3 h1">
                     Notter
                 </span>
-                <button className="btn-primary btn-add-new-post" onClick={() => {showCreatePost()}}> {btnText} </button>
-        </nav>
-        
-        {view ? <CreatePost/>:this}
+                {view?<AiOutlineCloseCircle size={30} onClick={showCreatePost}/>:<IoIosAddCircleOutline size={30} onClick={showCreatePost}/>}
+            </nav>
+
+            {view ? <CreatePost /> : this}
         </>
     )
 }
